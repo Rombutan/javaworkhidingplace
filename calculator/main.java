22192f0f75cab98bb2d22ff0f394cdc427908b3b
Luca Younes <lucaguyyounes@gmail.com>
2021-11-04 09:30:42 -0700

    methodize for no apparent reason

--- calculator/main.java
+++ calculator/main.java
@@ -8,21 +8,37 @@ class main {
 		char oper = scan.nextLine().charAt(0);
 		System.out.println("Enter second number");
 		double num2 = Double.parseDouble(scan.nextLine());
+		doOper(num1,oper,num2);
+    }
+	private static void doOper(double num1, char oper, double num2) {
 		switch(oper) {
 			case '+':
-				System.out.println(num1+num2);
+				System.out.println(add(num1,num2));
 				break;
-			case '-': 
-				System.out.println(num1-num2);
+			case '-':
+				System.out.println(subtract(num1,num2));
 				break;
 			case '/':
-				System.out.println(num1/num2);
+				System.out.println(divide(num1,num2));
 				break;
 			case '*':
-				System.out.println(num1*num2);
+				System.out.println(multiply(num1,num2));
 				break;
 			default:
 				System.out.println("You fucking failure");
-		}
+        }
+    }
+
+    	private static double add(double num1, double num2){
+		return(num1+num2);
+	}
+	private static double subtract(double num1, double num2){
+		return(num1-num2);
+	}
+	private static double multiply(double num1, double num2){
+		return(num1*num2);
+	}
+	private static double divide(double num1, double num2){
+		return(num1/num2);
 	}
 }
