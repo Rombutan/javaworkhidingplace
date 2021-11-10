7415f02a65d12d0c26cb2f2106cdc8217fa4bbef
Luca Younes <lucaguyyounes@gmail.com>
2021-11-10 09:02:22 -0800

    stuffs

---
+++ calculator/spell.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class spell {
+	public static void main (String[] args) {
+		
+	}
+}
+
+private class spelllist {
+	String spellfile = "words.txt";
+	public static void importList() {
+		Scanner spellListScan = new Scanner(
+	}
+}
+
